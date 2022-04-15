255c18598135e91d3f8e36cf2db8f5f2e6499d27
samiuljoy <samiuljoy46@gmail.com>
2022-04-15 16:01:14 +0600

    update

---
+++ js/toggle.js
@@ -0,0 +1,30 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#switch');
+var word2 = document.getElementById("sword");
+var meta = document.querySelector("meta[name=theme-color]");
+
+const enableDarkMode = () => {
+	document.body.classList.add('D');
+	meta.setAttribute("content", "#000000");
+	//word2.innerHTML = '🌙';
+	word2.innerHTML = 'τ';
+	localStorage.setItem('darkMode', 'enabled');
+}
+const disableDarkMode = () => {
+	document.body.classList.remove('D');
+	meta.setAttribute("content", "#002f30");
+	//word2.innerHTML = '🔆';
+	word2.innerHTML = 'λ';
+	localStorage.setItem('darkMode', null);
+}
+if (darkMode === 'enabled') {
+	enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+	darkMode = localStorage.getItem('darkMode');
+	if (darkMode !== 'enabled') {
+		enableDarkMode();
+	} else {
+		disableDarkMode();
+	}
+});
