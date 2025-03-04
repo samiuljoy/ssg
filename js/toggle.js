3da3dfd86defbf3224fbecd449908b017a8ce98e
samiuljoy <samiuljoy46@gmail.com>
2025-03-05 04:54:47 +0600

    update on the color scheme so that website can load colors based on user preference first

--- js/toggle.js
+++ js/toggle.js
@@ -1,18 +1,9 @@
 // Assigning dark mode values to 2 variables
 let darkMode = localStorage.getItem('darkMode');
-//let dark_value = localStorage.getItem('dark_value');
 // class and theme set
 const darkModeToggle = document.querySelector('#switch');
 var word2 = document.getElementById("sword");
 var meta = document.querySelector("meta[name=theme-color]");
-// Will load color scheme based on browser preference
-if (! localStorage.darkMode) {
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		meta.setAttribute("content", "#000000");
-		word2.innerHTML = 'τ';
-		document.body.classList.add('D');
-	}
-}
 // dark mode enable function
 const enableDarkMode = () => {
 	document.body.classList.add('D');
@@ -31,14 +22,18 @@ const disableDarkMode = () => {
 if (darkMode === '1') {
 	enableDarkMode();
 }
+// Will load color scheme based on browser preference
+if (! localStorage.darkMode) {
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		enableDarkMode();
+	}
+}
 // the click function
 darkModeToggle.addEventListener('click', () => {
 	darkMode = localStorage.getItem('darkMode');
 	if (darkMode !== '1') {
 		enableDarkMode();
-//		localStorage.setItem('dark_value', '1');
 	} else {
 		disableDarkMode();
-//		localStorage.setItem('dark_value', null);
 	}
 });
